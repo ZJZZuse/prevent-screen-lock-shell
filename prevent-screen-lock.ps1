c27ee24fa8ffadf99a583ccc92ee149add1d5df0
zeus <447338871@qq.com>
2019-10-28 16:09:50 +0800

    Add-Accelerator

--- prevent-screen-lock.ps1
+++ prevent-screen-lock.ps1
@@ -1,3 +1,5 @@
+Add-Accelerator SendKeys           System.Windows.Forms.SendKeys       -EA SilentlyContinue
+
 # 你可以根据你的实际情况修改间隔时间
 New-Variable -name INTERVAL -Value (60*3) -Option Constant -Description 'for 5mins lock default'
 
@@ -8,6 +10,6 @@ while ($true) {
     $key = '{BREAK}'
     get-date
     Write-Information "press key $key`n"
-    [System.Windows.Forms.SendKeys]::SendWait($key)
+    [SendKeys]::SendWait($key)
     sleep -s $INTERVAL
 }
