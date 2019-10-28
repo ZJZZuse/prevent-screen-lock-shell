7c48259963caae5aaecf415888feaa4ed6916a3b
zeus <447338871@qq.com>
2019-10-28 16:05:12 +0800

    去掉依赖

--- prevent-screen-lock.ps1
+++ prevent-screen-lock.ps1
@@ -1,15 +1,13 @@
-Import-Module Wasp
-
 # 你可以根据你的实际情况修改间隔时间
 New-Variable -name INTERVAL -Value (60*3) -Option Constant -Description 'for 5mins lock default'
 
 get-date
-echo `start`
+Write-Information `start`
 
 while ($true) {
     $key = '{BREAK}'
     get-date
-    echo "press key $key`n"
-    Send-UIKeys -keys $key
+    Write-Information "press key $key`n"
+    [System.Windows.Forms.SendKeys]::SendWait($key)
     sleep -s $INTERVAL
 }
